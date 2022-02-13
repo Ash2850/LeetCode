e5d5f06537f5e03a129d1f93312838333948debf
Ash2850 <99252545+Ash2850@users.noreply.github.com>
2022-02-13 13:29:27 +0530

    Time: 12 ms (14.81%), Space: 53.3 MB (5.06%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 1) return strs[0];
+        if(strs[0].length() == 0) return strs[0];
+        int maxLCP = 0;
+        String exp = new String(strs[0]);
+        int count = 0;
+        int N = strs[0].length();
+        int i;
+        for(i = 0; i< N; i++){
+            count = 0;
+            for(String s : strs){
+                String sub = new String();
+                if(i+1 <= s.length()){
+                    sub = s.substring(0, i+1);
+                }
+                //System.out.println("Substring checked is "+ exp.substring(0,i+1));
+                if(sub.contains(exp.substring(0, i+1))){
+                    count ++;
+                    //System.out.println(String.format("Value of count is %d", count));
+                }
+            }    
+            if(count == strs.length){
+                maxLCP = i;
+                //System.out.println(String.format("Value of maxLCP is %d", maxLCP));
+            }
+            else if(count < strs.length && i == 0){ return new String();}
+        }
+        return new String(exp.substring(0, maxLCP+1));
+    }
+}
